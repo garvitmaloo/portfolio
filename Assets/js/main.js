d4b5c143106cc4cd77d84a17a2c910ea580f748a
garvit <garvitmaloo.21@gmail.com>
2023-01-26 12:30:01 +0530

    Added hero section and did minor improvements in side-nav

---
+++ Assets/js/main.js
@@ -0,0 +1,8 @@
+// Working of dynamic typing in the hero section
+new Typed(".dynamic-type", {
+  strings: ["Web Developer", "Web Designer", "Freelancer"],
+  loop: true,
+  typeSpeed: 80,
+  backSpeed: 40,
+  backDelay: 2000,
+});
